cf26f9291b46e6674fcd0acf423ef776ed2b721c
Lebron <wenzhi.zeng@outlook.com>
2021-11-22 23:13:25 +0800

    test

--- www/izettle.js
+++ www/izettle.js
@@ -1,33 +1,41 @@
-var exec = require('cordova/exec');
+function iZettle() {}
 
 var PLUGIN_NAME = "iZettle";
 
-module.exports = {
-  initialize: function (clientId, callbackURL, success, error) {
-    exec(success, error, PLUGIN_NAME, "initialize", [clientId, callbackURL]);
-  },
+iZettle.prototype.initialize = function (clientId, callbackURL, success, error) {
+  cordova.exec(success, error, PLUGIN_NAME, "initialize", [clientId, callbackURL]);
+  };
 
-  charge: function (amount, reference, success, error) {
-    exec(success, error, PLUGIN_NAME, "charge", [amount, reference]);
-  },
+  iZettle.prototype.charge = function (amount, reference, success, error) {
+    cordova.exec(success, error, PLUGIN_NAME, "charge", [amount, reference]);
+  };
 
-  refund: function (amount, reference, refundReference, success, error) {
-    exec(success, error, PLUGIN_NAME, "refund", [amount, reference, refundReference]);
-  },
+iZettle.prototype.refund = function (amount, reference, refundReference, success, error) {
+  cordova.exec(success, error, PLUGIN_NAME, "refund", [amount, reference, refundReference]);
+  };
 
-  retrievePaymentInfo: function (reference, success, error) {
-    exec(success, error, PLUGIN_NAME, "retrievePaymentInfo", [reference]);
-  },
+iZettle.prototype.retrievePaymentInfo = function (reference, success, error) {
+  cordova.exec(success, error, PLUGIN_NAME, "retrievePaymentInfo", [reference]);
+  };
 
-  presentSettings: function (success, error) {
-    exec(success, error, PLUGIN_NAME, "presentSettings", null);
-  },
+iZettle.prototype.presentSettings = function (success, error) {
+  cordova.exec(success, error, PLUGIN_NAME, "presentSettings", null);
+  };
 
-  enforceAccount: function (email, clientId, callbackURL, success, error) {
-    exec(success, error, PLUGIN_NAME, "enforceAccount", [email, clientId, callbackURL]);
-  },
+iZettle.prototype.enforceAccount = function (email, clientId, callbackURL, success, error) {
+  cordova.exec(success, error, PLUGIN_NAME, "enforceAccount", [email, clientId, callbackURL]);
+  };
 
-  logout: function (success, error) {
-    exec(success, error, PLUGIN_NAME, "logout", null);
-  },
+iZettle.prototype.logout = function (success, error) {
+  cordova.exec(success, error, PLUGIN_NAME, "logout", null);
+  };
+
+iZettle.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.iZettle = new iZettle();
+  return window.plugins.iZettle;
 };
+
+cordova.addConstructor(iZettle.install);
